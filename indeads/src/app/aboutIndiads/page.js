1eb4855735a276369c45605c5369596ac2d29c77
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2023-08-26 15:49:57 +0530

    about InDiAds complelted by Kushal Rastogi

--- indeads/src/app/aboutIndiads/page.js
+++ indeads/src/app/aboutIndiads/page.js
@@ -3,7 +3,44 @@ import styles from "../page.module.css"
 export default function aboutIndiads(){
     return(
         <main className={styles.main}>
+            
+            <h1 style="background-color:white;font-family:Display font;"><center>InDiAds</center></h1>
+            
+            <body bgcolor="#F6F8FB"></body>
+            
+    <p style="font-family:Helvetica;">
+               <ul>
+               <li>InDiAds is a creative content producing and advertising company that is part of INDIBUS.</li>
+               <li>We specialize in creating digital posters, interactive videos, and advertising them to target customer segments.</li>
+              
+               
+               </ul>
+               <p>Our team of experienced and creative professionals has a deep understanding of the digital marketing landscape. </p>
+               <p> We use our knowledge and expertise to create content that is engaging, informative, and relevant to your target audience.</p>
+               <p>We also work with influencers to create authentic and impactful marketing campaigns.</p>
+               <p>And we use social media to reach your target audience with targeted and personalized messages.</p>
+               
+               
+               <p><strong>We believe that every business has a unique story to tell.</strong></p> 
+               <p><strong>We help you tell your story in a way that will resonate with your target audience and achieve your marketing goals.</strong></p> 
+               
+               
+                <h3 style="font-family:Display fonts;"><strong>Our Services Include</strong></h3>
+               
+             <ul>
+               <i>
+                   <li>Content creation: We create high-quality content that is engaging and informative.</li>
+                   <li>Influencer marketing: We work with influencers to create authentic and impactful marketing campaigns.</li>
+                   <li>Social media marketing: We use social media to reach your target audience with targeted and personalized messages.</li>
+                   <li>Advertising: We place your ads in front of your target audience through a variety of channels, including search engine marketing, social media advertising, and display advertising.</li>
 
+               </i>
+
+               </ul>
+
+                <p style="font-family:Helvetic;">We are committed to providing our clients with the best possible service. We offer a free consultation so that you can learn more about what we do and how we can help you. 
+Contact us today to schedule a consultation.</p>
+                 </p>
         </main>
     )
-}
+  }
