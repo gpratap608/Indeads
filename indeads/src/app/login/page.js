2310fd6e86e29418240d8ccc71b9fe5f28022241
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-08-29 21:40:14 +0530

    Added a paragraph by Ganesh

--- indeads/src/app/login/page.js
+++ indeads/src/app/login/page.js
@@ -4,6 +4,7 @@ export default function login(){
     return(
         <main className={styles.main}>
             <h1 className={styles.Typewriter}> Login </h1>
+            <p className={styles.paragraph}> this is login page </p>
         </main>
     )
 }
