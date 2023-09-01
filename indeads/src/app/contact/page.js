c17d64e0196ade4f86b9ca1c27f622b88bcf37d3
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2023-09-01 16:22:05 +0530

    contact us page by Kushal Rastogi

--- indeads/src/app/contact/page.js
+++ indeads/src/app/contact/page.js
@@ -2,8 +2,35 @@ import styles from "../page.module.css"
 
 export default function Business(){
     return(
-        <main className={styles.main}>
-            <h1 className={styles.Typewriter}> Contact </h1>
+        <main className={styles.contact}>
+            <center>
+    <div >
+    <h2 >Contact Us</h2>
+    
+        <form className={styles.contactform}>
+        
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required/>
+            
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required/>
+            
+
+            <label for="subject">Subject:</label>
+            <input type="text" id="subject" name="subject" required/>
+            
+
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" ></textarea>
+            <br/>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+    </center>
+
         </main>
     )
 }
