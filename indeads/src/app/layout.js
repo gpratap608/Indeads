8a820aad833c5cdf89b38b49b68e9af65fc1fbe2
yuvraj-0777 <yuvsinh0777@gmail.com>
2023-09-04 00:18:50 +0530

    Layout temporary adjustments

--- indeads/src/app/layout.js
+++ indeads/src/app/layout.js
@@ -35,6 +35,9 @@ export default function RootLayout({ children }) {
                     <div className='flexContent' ><Link href='/Indilearn' className='pagelink'> InDiLearn </Link></div>
                     <div className='flexContent' ><Link href='/signup' className='pagelink'> Signup </Link></div>
                     <div className='flexContent' ><Link href='/login' className='pagelink'> Login </Link></div>
+                    <div className='flexContent' ><Link href='/dashboard' className='pagelink'> Dashboard </Link></div> {/* Temporary Addition */}
+                    <div className='flexContent' ><Link href='/error' className='pagelink'> Error </Link></div>  {/* Temporary Addition */}
+
                 </div>
             </div>
             <div className='item mainbody'>
