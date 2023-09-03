4fbd5da4f7f9626dc3e93b686ae4cf91391df301
yuvraj-0777 <yuvsinh0777@gmail.com>
2023-09-04 00:14:05 +0530

    Error

---
+++ indeads/src/app/error/page.js
@@ -0,0 +1,9 @@
+import styles from"../page.module.css"
+
+export default function error(){
+    return(
+        <main className={styles.main}>
+            <h1 className={styles.Typewriter}> Error </h1>
+        </main>
+    )
+}
