9f81cf972a85f5d1304b821db518af8ee18a6d68
mr_varshney2004 <vivekvarshney2004@gmail.com>
2023-09-04 15:40:37 +0530

    Added error content by vivek

--- indeads/src/app/error/page.js
+++ indeads/src/app/error/page.js
@@ -1,9 +1,22 @@
-import styles from"../page.module.css"
+import styles from "../page.module.css"
 
-export default function error(){
-    return(
+export default function error() {
+    return (
         <main className={styles.main}>
-            <h1 className={styles.Typewriter}> Error </h1>
+            <div className={styles.errorPage}>
+                <div className={styles.errorContent}>
+                    <h1>404</h1>
+                    <h4>Opps! Page not found </h4>
+                    <p>Sorry, the page you're looking for doesn't exist. if you think something is broken, report a problem</p>
+                    <div className={styles.btns}>
+                        <a href="#">return home</a>
+                        <a href="#">report problem</a>
+
+                    </div>
+
+                </div>
+            </div>
+
         </main>
     )
 }
