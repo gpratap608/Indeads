1e311ca33ded6cecaffd0cd46b4dae33d6f52ca8
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-09-06 10:52:39 +0530

    Test Changes

--- indeads/src/components/Userinfo.js
+++ indeads/src/components/Userinfo.js
@@ -4,19 +4,23 @@
 import { signOut } from "next-auth/react"
 import styles from "../app/page.module.css"
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 export default function Userinfo(){
-    
+    const router = useRouter()
     const {data:session} = useSession()
 
-    
+    const handleClick = async () =>{
+        await signOut()
+        router.push("/login")
+    }
 
     return(
     <main className={styles.signup}>
             <h1 className={styles.signupHead}> Dashboard </h1>
             <p className={styles.signupPageInput}> {session?.user?.name} </p> 
             <p className={styles.signupPageInput}> {session?.user?.email} </p> 
-            <button onClick={()=>signOut()} className={styles.signupButton}> LogOUT </button>
+            <button onClick={handleClick} className={styles.signupButton}> LogOUT </button>
 
     </main>
     )
