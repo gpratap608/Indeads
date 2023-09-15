1547912821ed3a8ef1e876d3aef9d0b8d7b70979
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-09-15 20:48:46 +0530

    Completed IndiLearn

--- indeads/src/app/Indilearn/page.js
+++ indeads/src/app/Indilearn/page.js
@@ -1,51 +1,58 @@
 import styles from "../page.module.css"
 
+
 export default function Indilearns() {
     return(
         <main className={styles.main}>
-               
-            <center><h1 className={styles.learn}> InDiLearn  </h1></center> 
-
-<ul>
-<li>InDiLearn is an education-based startup that is part of INDIBUS</li>
-<li> We focus on developing the technical skills of young technocrats. </li>
-<li>We have a team of highly skilled individuals who are themselves part of very good projects and will help young technocrats to build awesome projects.</li>
-</ul>
+        <div className={styles.center}> <div className={styles.flexconstent}><h1 className='in'>In</h1><h1 className='di'>Di</h1><h1 className='bus'>Learn</h1></div></div>
+        
+        <div className={styles.pagecontentbackground}>
 
 
-<h3><strong>Mission</strong></h3>
-<i>
+        <p>
+            <i><b>InDiLearn</b></i> is an education-based startup that is part of INDIBUS
+            We focus on developing the technical skills of young technocrats. 
+            We have a team of highly skilled individuals who are themselves part of very good projects and will help young technocrats to build awesome projects.<br></br>
+        </p>
+        <h3><strong>Mission</strong></h3>
+        <p>
+            Our mission is to empower young technocrats to achieve their full potential in the technology industry.
+            We believe that everyone has the ability to learn and grow, and we are here to help them reach their goals.
 
-<p>Our mission is to empower young technocrats to achieve their full potential in the technology industry.</p>
-<p>We believe that everyone has the ability to learn and grow, and we are here to help them reach their goals.</p>
-</i>
-<h3 ><strong>Our Goals are-:</strong></h3>
+        </p>
+        <p>
+            
+            Provide young technocrats with the skills and knowledge they need to succeed in the technology industry.
+            Help young technocrats develop the critical thinking and problem-solving skills they need to be successful.
+            Connect young technocrats with mentors and opportunities in the technology industry.
+        </p>
+        <p>
+            We offer a variety of online courses and bootcamps that cover a wide range of topics, from software development to data science to cybersecurity.
+            Our courses are taught by experienced instructors who are passionate about teaching and helping students succeed.
+        </p>
+        <p>
+            We believe that the best way to learn is by doing, so we offer our students a variety of hands-on learning opportunities.  
+            Our students have the opportunity to work on real-world projects, collaborate with other students, and get feedback from their instructors.
+            We are committed to providing our students with the best possible learning experience.
+            We offer a variety of support services, including one-on-one tutoring, career counseling, and job placement assistance.
+        </p>
+        <p>
+            We are excited about the future of InDiLearn.
+            We believe that we have the potential to make a real difference in the lives of young technocrats by helping them achieve their goals in the technology industry.
 
-<ul>
-<i>
-    <li>Provide young technocrats with the skills and knowledge they need to succeed in the technology industry.</li>
-    <li>Help young technocrats develop the critical thinking and problem-solving skills they need to be successful.</li>
-    <li>Connect young technocrats with mentors and opportunities in the technology industry.</li>
-    </i> 
-    </ul>
 
-<p>We offer a variety of online courses and bootcamps that cover a wide range of topics, from software development to data science to cybersecurity.</p> 
-<p> Our courses are taught by experienced instructors who are passionate about teaching and helping students succeed.
-</p>
-<p>We believe that the best way to learn is by doing, so we offer our students a variety of hands-on learning opportunities. </p> 
-<p>Our students have the opportunity to work on real-world projects, collaborate with other students, and get feedback from their instructors.</p>
- <p>We are committed to providing our students with the best possible learning experience.</p>
- <p>We offer a variety of support services, including one-on-one tutoring, career counseling, and job placement assistance.
-</p>
- <p>We are excited about the future of InDiLearn.</p>
- <p>We believe that we have the potential to make a real difference in the lives of young technocrats by helping them achieve their goals in the technology industry.
-
-</p>
- <p>We are confident that you will be happy with our services.</p>
- <p> We offer a free consultation so that you can learn more about what we do and how we can help you. 
-</p>
-
-    <p>Contact us today to schedule a consultation.</p>
-        </main>
+        </p>    
+    
+        <p>
+            We are confident that you will be happy with our services.
+            We offer a free consultation so that you can learn more about what we do and how we can help you. 
+        </p>
+        <hr/>
+        <p>
+            Contact us today to schedule a consultation.
+        </p>
+    </div>
+        
+    </main>
     )
 }
