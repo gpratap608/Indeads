01f5aa1a786b01669dba4de8949b675e14f51c51
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-09-16 12:23:00 +0530

    Management changes

--- indeads/src/app/profile/page.js
+++ indeads/src/app/profile/page.js
@@ -7,7 +7,7 @@ import axios from "axios"
 import Link from "next/link"
 import {toast} from "react-hot-toast"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 
 
@@ -26,25 +26,14 @@ export default function Userinfo(){
             toast.error(error.message)
         }
     }
-
-    async function logout() {
-        try {
-            await axios.get('/api/users/logout')
-            toast.success('Logout Successful')
-            router.push("/login")
-        } catch (error) {
-            console.log(error.message)
-            toast.error(error.message)
-        }
-    }
-
     const getUserDetails = async () => {
         const res = await axios.get("/api/users/me")
         console.log(res.data)
         setData(res.data.data.userName)
         setEmail(res.data.data.email)
     }
-    return(
+    getUserDetails()
+    return( 
     <main className={styles.main}>
         <div className={styles.signupsetup}>
         <div className={styles.orangeline}></div>
