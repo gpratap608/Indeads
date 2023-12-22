86c847cb3e06c2e1c5872527de5ca2f2d4016c29
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-12-23 00:30:40 +0530

    Update page.js

--- indeads/src/app/page.js
+++ indeads/src/app/page.js
@@ -18,56 +18,6 @@ export default function Home() {
   const router = useRouter();
   return (
     <main className={styles.main}>
-        /*<div className={styles.upper}>
-          <div className={styles.flexContainerimage}>
-              <div className={styles.flexContentimage}>
-                    <div className={styles.Image}>
-                      <Corouseldata/>  
-                    </div>
-              </div>
-        </div>
-
-        <h3 className='head'>Our Excited Services Offerings</h3>
-        <div className={styles.fleximageContainer}>
-          <div className={styles.fleximage}>
-            <Image
-              src='/indiads1.png'
-              height='200'
-              width='200'
-              alt='Image'
-            />
-          </div>
-          <div className={styles.fleximage}>
-            <Image
-              src='/indicollab.png'
-              height='200'
-              width='200'
-              alt='Image'
-            />
-          </div>
-          <div className={styles.fleximage}>
-            <Image
-              src='/indilearn.png'
-              height='200'
-              width='200'
-              alt='Image'
-            />
-          </div>
-          
-          
-          <button><img
-            src='/arrowright.png'
-            height='50'
-            width='50'
-
-          /></button>
-
-
-        </div>
-      </div>
-      <Bodycontent/>
-      {/* <Reviews/> 
-<Footer/>*/
       <Maintenance/>
     </main>
   )
